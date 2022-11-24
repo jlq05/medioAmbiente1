f5da9daeac4c106a53bb2f986ce1492eb4cfe730
jlq05 <jose_lq05@live.com.ar>
2022-11-24 11:23:13 -0300

    mas cambios

--- src/main/java/main/Routes.java
+++ src/main/java/main/Routes.java
@@ -17,8 +17,15 @@ import spark.debug.DebugScreen;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class Routes {
+
+  public static Routes INSTANCE = new Routes();
+
+  public static Routes getInstance() {
+    return INSTANCE;
+  }
+
   public static void main(String[] args) {
-    Spark.port(getHerokuAssignedPort());
+
     Spark.staticFileLocation("/path/to/sources");
     System.out.println("Iniciando el servidor...");
     Spark.port(9001);
@@ -101,11 +108,5 @@ public class Routes {
 
   }
 
-  public static int getHerokuAssignedPort() {
-    ProcessBuilder processBuilder = new ProcessBuilder();
-    if (processBuilder.environment().get("PORT") != null) {
-      return Integer.parseInt(processBuilder.environment().get("PORT"));
-    }
-    return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
-  }
+
 }
